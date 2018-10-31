34d38df2d6669580eb4b974c95a6f9907fac88cb
Nash Kissel <nashkissel@gmail.com>
2018-10-31 14:09:54 -0400

    Add project files.

---
+++ Stars Problem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stars_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+           for (int i = 0; i < answer; i++)
+            {
+               
+                for (int x = 0; x<= i; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
